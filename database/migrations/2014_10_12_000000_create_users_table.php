a22f781eabbad4a1f3cea2323dd3af2a99d3c6ad
Nelson Ameyo <nelson@sodium.co.ke>
2021-04-15 12:23:39 +0300

    Adding picture to user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('picture')->nullable();
             $table->string('email')->unique();
             $table->string('telephone')->unique();
             $table->string('next_appointment')->nullable();
